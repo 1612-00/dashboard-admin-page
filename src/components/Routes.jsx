c34edf7eb251e900b534984a2e9a4d7448cd301d
1612-00 <nguyenducanh.ldb@gmail.com>
2021-12-02 15:49:41 +0700

    completed topnav

---
+++ src/components/Routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from './../pages/Dashboard';
+import Customer from './../pages/Customer';
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route path='/' exact component={Dashboard} />
+            <Route path='/customer' component={Customer} />
+        </Switch>
+    );
+};
+
+export default Routes;
